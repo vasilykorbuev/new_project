0feaeed87f49e5666e5a98d7fc2ff9452cd18f1a
sew_online <sew_online@mail.ru>
2021-12-12 00:22:32 +0500

    first commit_example

---
+++ js/script.js
@@ -0,0 +1,11 @@
+"use strict";
+
+// const answers = [];
+// answers[0] = prompt('Как ваше имя?', '');
+// answers[1] = prompt('Как ваша фамилия?', '');
+// answers[2] = prompt('Сколько вам лет?', '');
+
+// document.write(answers);
+
+const user = prompt("Как ваше имя?");
+alert(`Привет, ${user}`); // Это - бектики
